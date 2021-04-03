f5fdf941d2649cdb01db3156d06b3f956542c53c
lopatuhka <helena.lopatukhina@gmail.com>
2021-04-03 15:31:14 +0300

    init main

---
+++ main.py
@@ -0,0 +1,10 @@
+import CytotoxicityExperiment as ce
+path_to_file = 'C:/Users/acer/Desktop/Work/test_data.xlsx'
+
+exp = ce.CytotoxicityAssay()
+#
+# exp.read_data(path_to_file)
+#
+# print(exp.__data)
+
+print('Hello')
