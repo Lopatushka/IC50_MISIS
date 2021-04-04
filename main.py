e1e81d85fdea91b4c36738c4fbd163d06773c6e9
lopatuhka <helena.lopatukhina@gmail.com>
2021-04-04 15:46:08 +0300

    delete main. class CytotoxicityExperiment init

---
+++ main.py
@@ -0,0 +1,45 @@
+import CytotoxicityExperiment as exp
+path_to_file = 'C:/Users/acer/Desktop/Work/Data/MTS/18.12.20_MTS/HEK_plate_1.xlsx'
+
+# Import data
+df = exp.CytotoxicityAssay()
+df.read_data(path_to_file)
+
+# Delete blank rows
+df.delete_rows(colname='Тип', to_delete=['blank'])
+
+# Information about dataset
+print('Size of data:', df.get_data().shape)
+print('All drugs:', df.list_of_drugs(include_controls=True))
+print('Control drugs:', df.list_of_controls())
+print('Wavelengths:', df.list_of_wlengths())
+
+# Substract background
+df.substract_background(490, 700)
+
+# Add concentrations
+df.add_concentration(axis='vertical', n_of_steps=8,
+                         drugs_dict={"MS309": [100, 25], "MMP58": [100, 25], "MS306": [100, 25]},
+                         log_scale=True)
+# Checking dataframe
+print(df.get_data().head(16))
+
+# Normalization to controls
+    df.normalization(control_dict={}, digits=3)
+
+    # Drop controls
+    df.drop_control()
+
+    # Subset
+    #sb = df.subset(drug='MS-1')
+    #print(sb)
+
+    # Reshape
+    results = df.reshape()
+    #print(results)
+
+    # Export to excel
+    results.to_excel('C:/Users/acer/Desktop/Work/test_results.xls')
+
+    # print(df.get_data().head(3))
+    # print(df.get_data().tail(3))
