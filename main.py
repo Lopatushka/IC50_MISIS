0102aa8baae0c75e935fc2dcd27c96621fe68f4c
lopatuhka <helena.lopatukhina@gmail.com>
2021-04-05 11:25:17 +0300

    drop_control(): add ValueError,  change docs

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 import CytotoxicityExperiment as exp
+
 HEK_exp = ['C:/Users/acer/Desktop/Work/Data/MTS/18.12.20_MTS/HEK_plate_1.xlsx',
            'C:/Users/acer/Desktop/Work/Data/MTS/18.12.20_MTS/HEK_plate_2.xlsx']
 
-# Import data
+# Import data & delete blank rows
 df = exp.CytotoxicityAssay()
 df.read_data(HEK_exp)
-
-print(df.get_data().columns)
-
-# Delete blank rows
 df.delete_rows(colname='Тип', to_delete=['blank'])
-
-# Information about dataset
-print('Size of data:', df.get_data().shape)
-print('Plates:', df.list_of_plates())
-print('All drugs:', df.list_of_drugs(include_controls=True))
-print('Control drugs:', df.list_of_controls())
-print('Wavelengths:', df.list_of_wlengths())
 
 # Substract background
 df.substract_background(490, 700)
 
+# Information about dataset
+print('Size of data:', df.get_data().shape)
+print('Plates:', df.list_of_plates())
+print('All drugs:', df.list_of_drugs(include_controls=False))
+print('Control drugs:', df.list_of_controls())
+print('Wavelengths:', df.list_of_wlengths())
+
 # Add concentrations
 df.add_concentration(axis='vertical', n_of_steps=8,
-                         drugs_dict={"MS309": [100, 25], "MMP58": [100, 25], "MS306": [100, 25]},
-                         log_scale=True)
-
+                     drugs_dict={"MS309": [100, 3],
+                                 "MMP58": [100, 3],
+                                 "MS306": [100, 3],
+                                 "MMAE": [0.5, 10]},
+                     exclude=['DMSO'],
+                     log_scale=False)
 
 # Normalization to controls
-    #df.normalization(control_dict={}, digits=3)
+df.normalization(control_dict={"MS309": "DMSO",
+                                "MMP58": "DMSO",
+                                "MS306": "DMSO",
+                                "MMAE": "DMSO"}, digits=3)
 
-    # Drop controls
-    #df.drop_control()
+# Drop controls
+df.drop_control(['DMSO'])
 
-    # Subset
-    #sb = df.subset(drug='MS-1')
-    #print(sb)
+# Subset
+#sb = df.subset(drug='DMSO')
+#print(sb)
 
-    # Reshape
-    #results = df.reshape()
-    #print(results)
+# Reshape
+results = df.reshape()
+print(results)
 
-    # Export to excel
-    #results.to_excel('C:/Users/acer/Desktop/Work/test_results.xls')
+# Export to excel
+results.to_excel('C:/Users/acer/Desktop/Work/Data/MTS/18.12.20_MTS/HEK_results.xlsx')
 
-    # print(df.get_data().head(3))
-    # print(df.get_data().tail(3))
+#print(df.get_data().head(16))
+#print(df.get_data().tail(16))
