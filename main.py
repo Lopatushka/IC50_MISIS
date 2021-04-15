7fa40c892969766ea7479a980d7d69cffc2f0e57
Lopatushka <helena,lopatukhina@gmail.com>
2021-04-15 13:26:43 +0300

    main: change

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import CytotoxicityExperiment as exp
-#
-path_to_file = ["C:/Users/User/Documents/Work/Data/MTS/12.04.21_MTS/12.04.21_MTS_MCF7.xlsx"]
-#
+
+path_to_file = ["C:/Users/User/Documents/Work/Data/MTS/18.12.20_MTS/HEK_plate_1.xlsx",
+                "C:/Users/User/Documents/Work/Data/MTS/18.12.20_MTS/HEK_plate_2.xlsx"]
+
 # Import data & delete blank rows
+# df = exp.CytotoxicityAssay()
+# data = pd.read_excel("C:/Users/User/Documents/Work/Data/MTS/09.04.21_MTS/row_data/pc3.xlsx")
+# df.set_data(data)
+#print(df.get_data())
+
+# Standard import
 df = exp.CytotoxicityAssay()
 df.read_data(path_to_file)
+#print(df.get_data())
 
-# Substract background
+#Substract background
 df.sub_bgrnd_single(wlength=490, wlength_to_subst=700)
 
 # Delete blank
@@ -20,24 +28,19 @@ print('Control drugs:', df.list_of_controls())
 print('Wavelengths:', df.list_of_wlengths())
 
 
-# Add concentrations
-df.add_concentration(drugs_dict={'DG4ClSe': [100, 3],
-                                 'DG603k': [100, 3],
-                                 'DG605k': [100, 2],
-                                 'DG618k': [100, 2],
-                                 })
+#Add concentrations
+df.add_concentration(drugs_dict={'MMAE': [0.5, 10], 'MS309': [100, 3],
+                                 'MMP58': [100, 3], 'MS306': [100, 3]})
 
 # Normalization
-df.normalization(control_dict={'DG4ClSe': 'DMSO-dil3',
-                               'DG603k': 'DMSO-dil3',
-                               'DG605k': 'DMSO-dil2', 'DG618k': 'DMSO-dil2'})
+df.normalization()
 
 
-# Drop control samples
-df.drop_control(control_names=['DMSO-dil2', 'DMSO-dil3'])
-
-print(df.reshape())
-
-# Export
-path_to_export = "C:/Users/User/Documents/Work/Data/MTS/12.04.21_MTS"
-exp.Export(data=df.reshape(), name='MCF7.xlsx', path_to_dir=path_to_export)
+# # Drop control samples
+# df.drop_control()
+#
+# print(df.reshape())
+#
+# # # Export
+# path_to_export = "C:/Users/User/Documents/Work/Data/MTS/09.04.21_MTS"
+# exp.Export(data=df.reshape(), name='pc3.xlsx', path_to_dir=path_to_export)
